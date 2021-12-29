03d073371b64c468e57d3c27e45017533186af79
Anna <annasmelekhina@gmail.com>
2021-12-29 20:37:23 +0300

    Initial Commit

---
+++ ContactApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactApp
+//
+//  Created by Анна Мелехина on 29.12.2021.
+//  Copyright © 2021 Anna Melekhina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
